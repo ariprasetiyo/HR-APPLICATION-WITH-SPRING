7de85baaf35b2c6604c0378889d0d244e2006ae3
Ari Prasetiyo <prasetiyooo@gmail.com>
2016-12-11 11:23:27 +0700

    Changes to day before take a bath and will going to warteg

---
+++ src/main/java/ari/com/hr/application/model/SysMenus.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ari.com.hr.application.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author ari-prasetiyo
+ */
+@Entity
+@Table(name = "sys_menu")
+public class SysMenus extends ModelSerializable {
+
+    private String menusName;
+    private String url;
+
+    public String getMenusName() {
+        return menusName;
+    }
+
+    public void setMenusName(String menusName) {
+        this.menusName = menusName;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
