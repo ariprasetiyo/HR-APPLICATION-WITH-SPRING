c601e98de048954b8d19aa366bc3b8e766e9fe8e
Ari Prasetiyo EBC <prasetiyooo@gmail.com>
2017-02-19 11:02:04 +0700

    repair rest api authorization

--- src/main/java/ari/com/hr/application/controller/rest/authorization/AuthorizationRestController.java
+++ src/main/java/ari/com/hr/application/controller/rest/authorization/AuthorizationRestController.java
@@ -2,11 +2,16 @@ package ari.com.hr.application.controller.rest.authorization;
 
 import ari.com.hr.application.dao.SysAuthorizationDao;
 import ari.com.hr.application.dto.GlobalDto;
+import ari.com.hr.application.dto.SysAuthorizationDto;
 import ari.com.hr.application.model.SysAuthorization;
+import java.util.ArrayList;
+import java.util.List;
+import javax.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,40 +49,40 @@ public class AuthorizationRestController {
         return new ResponseEntity(globalDto, HttpStatus.OK);
     }
 
-    @RequestMapping("/addMenu/{idRoles}")
-    @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<SysAuthorization> authorizationAddMenu(@PathVariable("idRoles") Integer id,
+    @RequestMapping(value = "/addMenu/{idRoles}", method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @Transactional
+    public SysAuthorizationDto authorizationAddMenu(@PathVariable("idRoles") Long idRole,
             @RequestParam("vInsert") boolean vInsert,
             @RequestParam("vUpdate") boolean vUpdate,
             @RequestParam("vDelete") boolean vDelete,
             @RequestParam("vDisable") boolean vDisable,
-            @RequestParam("modelMenuId") Integer MenuId,
-            @RequestParam(value = "modelParentMenuId", required = false) Long parentMenuId) {
-
-        SysAuthorization dataAuthorization = saveDataMenu(id, vInsert, vUpdate,
+            @RequestParam("modelMenuId") Long MenuId,
+            @RequestParam("modelParentMenuId") Long parentMenuId) {
+        return saveDataMenu(idRole, vInsert, vUpdate,
                 vDelete, vDisable, MenuId, parentMenuId);
-        return new ResponseEntity(dataAuthorization, HttpStatus.OK);
     }
 
-    @RequestMapping("/deleteMenu/{idAuthorization}")
+    @RequestMapping(value = "/deleteMenu/{idAuthorization}", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<GlobalDto> deleteMenu(@PathVariable("idAuthorization") Long id) {
         dsSysAuthorization.delete(id);
         GlobalDto globalDto = new GlobalDto();
         globalDto.setId(id);
+
         return new ResponseEntity(globalDto, HttpStatus.OK);
     }
 
-    private SysAuthorization saveDataMenu(Integer id, boolean vInsert,
+    private SysAuthorizationDto saveDataMenu(Long idRole, boolean vInsert,
             boolean vUpdate, boolean vDelete,
-            boolean vDisable, Integer MenuId,
+            boolean vDisable, Long MenuId,
             Long parentMenuId) {
 
         SysAuthorization dataAuthorization = new SysAuthorization();
-        log.debug("-add new menu on id " + id
+        log.debug("-add new menu on id " + idRole
                 + ", menuId : " + MenuId
                 + ", parentId " + parentMenuId
-                + " " + vInsert + "" + vUpdate + "" + vDelete + "" + vDisable);
+                + " " + vInsert + " " + vUpdate + " " + vDelete + " " + vDisable);
         dataAuthorization.setSysMenu(MenuId);
 
         if (parentMenuId == null) {
@@ -86,18 +91,20 @@ public class AuthorizationRestController {
             dataAuthorization.setParent(parentMenuId);
         }
 
-        dataAuthorization.setSysRoles(id);
+        dataAuthorization.setSysRoles(idRole);
         dataAuthorization.setIsDelete(vDelete);
         dataAuthorization.setIsInsert(vInsert);
         dataAuthorization.setIsUpdate(vUpdate);
         dataAuthorization.setDisabled(vDisable);
         dataAuthorization.setIsRead(true);
+
         dataAuthorization = dsSysAuthorization.save(dataAuthorization);
+        log.debug("new id menu after add : " + dataAuthorization.getId() + "--");
 
-        return getDataAuthorizationById(dataAuthorization.getId());
-    }
+        SysAuthorizationDto dataAuthorizations = dsSysAuthorization.getDataAuthorizationById(dataAuthorization.getId());
+        log.debug("new id menu after get :" + dataAuthorizations.getId() + "--");
+        log.debug("menu name after add : " + dataAuthorizations.getMenuName());
 
-    private SysAuthorization getDataAuthorizationById(Long idAuthorization) {
-        return dsSysAuthorization.getDataAuthorizationById(idAuthorization);
+        return dataAuthorizations;
     }
 }
