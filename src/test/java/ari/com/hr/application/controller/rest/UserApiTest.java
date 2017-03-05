53e54582ec5bda51924b4c6961cb423cfc69a8b4
Ari Prasetiyo EBC <prasetiyooo@gmail.com>
2017-03-05 10:21:54 +0700

    add function test api user list

---
+++ src/test/java/ari/com/hr/application/controller/rest/UserApiTest.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ari.com.hr.application.controller.rest;
+
+import org.apache.log4j.Logger;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ *
+ * @author ari-prasetiyo
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserApiTest {
+
+    Logger log = Logger.getLogger(UserApiTest.class);
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void testApiUserList() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(post("/admin/v1/api/user/list")
+                .param("start", "0")
+                .param("length", "25"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andReturn();
+    }
+}
